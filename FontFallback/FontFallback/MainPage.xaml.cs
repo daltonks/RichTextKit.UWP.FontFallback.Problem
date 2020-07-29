bbae00d23fabb5d4c8db4ba03cd842830aea045c
Dalton Spillman <dalton_spillman@yahoo.com>
2020-07-28 21:50:09 -0500

    Initial code commit

---
+++ FontFallback/FontFallback/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+using Topten.RichTextKit;
+using Xamarin.Forms;
+
+namespace FontFallback
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private readonly RichString _richString = new RichString("Emoji alert! 🌐🍪🍕🚀");
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            var canvas = e.Surface.Canvas;
+            canvas.Clear();
+            _richString.Paint(canvas, new SKPoint(100, 100));
+        }
+    }
+}
